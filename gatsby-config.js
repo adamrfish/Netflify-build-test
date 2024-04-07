f5a1c022af5390a9a0e46d72e8208e14c6a48b09
Adam <adam.ryan.fish@gmail.com>
2024-04-07 17:25:58 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Adam Gatsby Netlify Branch Test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
